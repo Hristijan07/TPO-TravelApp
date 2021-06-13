c0080b22c96f52d9e72ba1df5e435548dfa10729
Hristijan07 <hristijanvicoroski07@gmail.com>
2021-06-14 00:17:18 +0200

    local-database

--- travel_app/settings.py
+++ travel_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR_STAT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zz52unyxi=+eb@juw)gg*qrbal-__=u=1xhm&66l5gtdxqfinq'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
